2f59c439e2071d34f3786ef211733d450d06484c
LeThienTrungNguyen <khamnguyenakaams2001@gmail.com>
2024-06-08 11:40:45 +0700

    Update Enemy Drop Item By Total Value

--- Assets/_Scrip/ItemDrop/EnemyDropSpawner.cs
+++ Assets/_Scrip/ItemDrop/EnemyDropSpawner.cs
@@ -1,3 +1,5 @@
+using UnityEngine;
+
 public enum EnemyTag
 {
     Default,
@@ -11,13 +13,36 @@ public class EnemyDropSpawner : Spawner
 
     public static string DefaultDropItem = "ItemGem"; // Item 1
     public static string BossDropItem = "ItemGemBoss"; // Item 1
-
+    public int[] itemsValue;
     protected override void Awake()
     {
         base.Awake();
         EnemyDropSpawner.instance = this;
     }
+    public int testMaxValue;
+    [ContextMenu("TestMaxValue")]
+    public void TestMaxValue()
+    {
+        print("Test Started");
+        var maxValue = GetMaxItemValue(testMaxValue);
+        print("Test Completed ! Max Value:"+maxValue);
+    }
 
+    public int GetMaxItemValue(int total)
+    {
+        var itemsValue = new int[this.itemsValue.Length];
+        for (int i = 0 ;i < itemsValue.Length; i++)         {
+            itemsValue[i] = this.itemsValue[i];
+        }
+        var maxValue = int.MinValue;
+        foreach(var value in itemsValue)
+        {
+            if (value > total) continue;
+            if (value < maxValue) continue;
+            maxValue = value;
+        }
+        return maxValue;
+    }
     public string GetDropItemForEnemy(EnemyTag tag)
     {
         switch (tag)
--- Assets/_Scrip/Spawner/EnemyDeSpawn.cs
+++ Assets/_Scrip/Spawner/EnemyDeSpawn.cs
@@ -7,7 +7,12 @@ using UnityEngine.UIElements;
 public class EnemyDeSpawn : DespawnByTime
 {
     [SerializeField] protected EnemyCtrl enemyCtrl;
+    [SerializeField,Range(0,100)] private float SpawnRate;
+    [SerializeField] private float rate;
+    [SerializeField] private float startItemValueTotal;
     public EnemyCtrl EnemyCtrl => enemyCtrl;
+    public int minDropItemCount;
+    public int maxDropItemCount;
     protected override void LoadComponents()
     {
         base.LoadComponents();
@@ -19,12 +24,37 @@ public class EnemyDeSpawn : DespawnByTime
         this.enemyCtrl = transform.parent.GetComponent<EnemyCtrl>();
         Debug.Log(gameObject.name + ": loadEnemyCtrl" + gameObject);
     }
-    public override void deSpawnObjParent() 
+    public override void deSpawnObjParent()
     {
+        Debug.Log("Enemy Despawn");
         EnemySpawner.Instance.Despawn(transform.parent);
-        Transform newDropItem = EnemyDropSpawner.Instance.Spawn(GetTag(), transform.parent.position, Quaternion.identity);
+        var rate = Random.Range(0f, 100f);
+        var itemValueTotal = Random.Range(minDropItemCount, maxDropItemCount + 1);
+        startItemValueTotal = itemValueTotal;
+        if(rate <= SpawnRate)
+        {
+            while(itemValueTotal > 0)
+            {
+                Transform newDropItem = EnemyDropSpawner.Instance.Spawn(GetTag(), transform.parent.position, Quaternion.identity);
+
+                newDropItem.gameObject.SetActive(true);
+                var maxValue = EnemyDropSpawner.Instance.GetMaxItemValue(itemValueTotal);
+                itemValueTotal -= maxValue;
+                newDropItem.name = "Drop Item Value :" + maxValue;
+                switch (maxValue)
+                {
+                    case 1: { newDropItem.localScale *= 0.5f; break; }
+                    case 5: { newDropItem.localScale *= 0.8f; break; }
+                    case 10: { newDropItem.localScale *= 1f; break; }
+                    case 15: { newDropItem.localScale *= 1.5f; break; }
+                }
+                Debug.Log("Drop Item Value: " + maxValue + " by :" + transform.parent.name, transform.parent);
+            }
+                
+            
+            
+        }
         
-        newDropItem.gameObject.SetActive(true);
     }
     string GetTag()
     {
