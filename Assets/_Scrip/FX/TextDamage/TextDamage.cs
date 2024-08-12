b952011c243ba93e4befb8466e26674ff3339a41
LeThienTrungNguyen <khamnguyenakaams2001@gmail.com>
2024-08-12 13:18:01 +0700

    Add Text Damage

--- Assets/_Scrip/FX/TextDamage/TextDamage.cs
+++ Assets/_Scrip/FX/TextDamage/TextDamage.cs
@@ -1,11 +1,24 @@
-using System.Collections;
-using System.Collections.Generic;
+
 using TMPro;
 using UnityEngine;
+using DG.Tweening;
 
+public enum TextColorType
+{
+    none , fire , water , ground , axit 
+}
+
+public enum TextDamageAnimationType
+{
+    FadeInBigFirst, FadeOutBigFirst, FadeInSmallFirst, FadeOutSmallFirst
+}
 public class TextDamage : SaiMonoBehaviour
 {
     [SerializeField] protected TextMeshProUGUI text;
+    [SerializeField] private float textMoveDistance;
+    [SerializeField] private float animationDuration;
+    [SerializeField] private TextColorType typeColor;
+    [SerializeField] private TextDamageAnimationType typeAnimation;
 
     protected override void LoadComponents()
     {
@@ -17,6 +30,8 @@ public class TextDamage : SaiMonoBehaviour
     {
         if (this.text != null) return;
         this.text = GetComponentInChildren<TextMeshProUGUI>();
+
+
         Debug.LogWarning(transform.name + ": LoadText", gameObject);
     }
 
@@ -24,5 +39,95 @@ public class TextDamage : SaiMonoBehaviour
     {
         string damageNumber = LargeNumber.ToString(damage);
         this.text.text = damageNumber;
+
     }
+
+    private Vector3 hitPos;
+    float scaleStart = 1;
+    float scaleEnd = 2;
+    float alphaTextStart = 0;
+    float alphaTextEnd = 1;
+
+    public void DoAnimation(Vector3 startPosition)
+    {
+        this.hitPos = startPosition;
+
+        SetTextAnimationParameter(typeAnimation);
+        transform.DOMoveY(transform.position.y + textMoveDistance, animationDuration)
+            .OnStart(() =>
+        {
+            transform.position = startPosition;
+            text.color = GetTextColorByType(typeColor);
+            Debug.Log("On Start at : "+startPosition);
+        })
+            .OnComplete(() =>
+        {
+            Debug.Log("On Complete at:"+ transform.position.y + textMoveDistance);
+            transform.position = startPosition;
+            gameObject.SetActive(false);
+        })
+            .SetEase(Ease.Linear);
+        transform.DOScale(scaleStart, 0);
+        transform.DOScale(scaleEnd, animationDuration);
+        transform.GetComponentInChildren<TextMeshProUGUI>()
+                .DOFade(alphaTextStart, 0);
+        transform.GetComponentInChildren<TextMeshProUGUI>()
+            .DOFade(alphaTextEnd, animationDuration);
+
+    }
+    void SetTextAnimationParameter(TextDamageAnimationType type)
+    {
+        switch (type)
+        {
+            case TextDamageAnimationType.FadeInBigFirst: {
+                    scaleStart = 3;
+                    scaleEnd = 1;
+                    alphaTextStart = 0;
+                    alphaTextEnd = 1;
+                    break;
+                }
+            case TextDamageAnimationType.FadeOutBigFirst:
+                {
+                    scaleStart = 3;
+                    scaleEnd = 1;
+                    alphaTextStart = 1;
+                    alphaTextEnd = 0;
+                    break;
+                }
+            case TextDamageAnimationType.FadeInSmallFirst:
+                {
+                    scaleStart = 1;
+                    scaleEnd = 3;
+                    alphaTextStart = 0;
+                    alphaTextEnd = 1;
+                    break;
+                }
+            case TextDamageAnimationType.FadeOutSmallFirst:
+                {
+                    scaleStart = 1;
+                    scaleEnd = 3;
+                    alphaTextStart = 1;
+                    alphaTextEnd = 0;
+                    break;
+                }
+            default:
+                break;
+        }
+    }
+    public Color GetTextColorByType(TextColorType type)
+    {
+        Color color = Color.white;
+        switch (type)
+        {
+            case TextColorType.fire: color = Color.red; break;
+            case TextColorType.water: color = Color.cyan; break;
+            case TextColorType.ground: color = Color.gray + Color.red + Color.yellow; break;
+            case TextColorType.axit: color = Color.green; break;
+            default: color = Color.white; break;
+
+        }
+        Debug.Log("Type:"+type+",Color:"+color);
+        return color;
+    }
+
 }
