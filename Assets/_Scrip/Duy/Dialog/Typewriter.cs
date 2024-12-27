5a25aad9e150dcf77eefc82e7c741a61ec7a9bd1
KhangSPR <129411252+KhangSPR@users.noreply.github.com>
2024-12-27 21:33:41 +0700

    update

---
+++ Assets/_Scrip/Duy/Dialog/Typewriter.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+[RequireComponent(typeof(TextMeshProUGUI))]
+public class Typewriter : MonoBehaviour {
+
+    [SerializeField]
+    private TextMeshProUGUI textBox;
+
+    private Coroutine typewriterCoroutine;
+    private WaitForSeconds typewriterDelay;
+
+    private int currentVisibleCharacterIndex;
+
+    public bool isTyping;
+    [SerializeField] float speed;
+
+    private void Awake(){
+        textBox = GetComponent<TextMeshProUGUI>();
+    }
+
+    private void Start(){
+        isTyping = false;
+        typewriterDelay = new(1f/speed);
+    }
+
+    public void Set(string text){
+        if (typewriterCoroutine != null)
+            StopCoroutine(typewriterCoroutine);
+
+        textBox.text = text;
+        textBox.maxVisibleCharacters = 0;
+        currentVisibleCharacterIndex = 0;
+
+        typewriterCoroutine = StartCoroutine(Type());
+    }
+
+    public void Skip(){
+        if (!isTyping) return;
+        StopCoroutine(typewriterCoroutine);
+        textBox.maxVisibleCharacters = textBox.textInfo.characterCount;
+        isTyping = false;
+    }
+
+    private IEnumerator Type(){
+        isTyping = true;
+        var textInfo = textBox.textInfo;
+        while (currentVisibleCharacterIndex < textInfo.characterCount+1){
+            // var character = textInfo.characterInfo[currentVisibleCharacterIndex].character;
+            textBox.maxVisibleCharacters++;
+            yield return typewriterDelay;
+            currentVisibleCharacterIndex++;
+        }
+        isTyping = false;
+    }
+}
